072431a941fd3506e068217123b3cd9e7f5780ee
Jamal Javadov <camalcavadovwork@gmail.com>
2025-06-27 17:47:51 +0400

    Add files via upload

---
+++ products.js
@@ -0,0 +1,133 @@
+// products.js
+
+const products = [
+  {
+     image: "images/1.png",
+    price: "45 AZN",
+    material: "Gümüş 925",
+    tags: ["dəst", "bilərzik", "sep"]
+  },
+  {
+     image: "images/2.png",
+    price: "25 AZN",
+    material: "Gümüş 925",
+    tags: ["üzük"]
+  },
+  {
+     image: "images/3.png",
+    price: "26 AZN",
+    material: "Gümüş 925",
+    tags: ["sep"]
+  },
+  {
+     image: "images/4.png",
+    price: "30 AZN",
+    material: "Gümüş 925",
+    tags: ["bilərzik"]
+  },
+  {
+     image: "images/5.png",
+    price: "75 AZN",
+    material: "Gümüş 925",
+    tags: ["dəst","bilərzik","sep","üzük","sırğa"]
+  },
+  {
+     image: "images/6.png",
+    price: "27 AZN",
+    material: "Gümüş 925",
+    tags: ["üzük"]
+  },
+  {
+     image: "images/7.png",
+    price: "25 AZN",
+    material: "Gümüş 925",
+    tags: ["sırğa"]
+  },
+  {
+     image: "images/8.png",
+    price: "25 AZN",
+    material: "Gümüş 925",
+    tags: ["üzük"]
+  },
+  {
+     image: "images/9.png",
+    price: "25 AZN",
+    material: "Gümüş 925",
+    tags: ["üzük"]
+  },
+  {
+     image: "images/10.png",
+    price: "26 AZN",
+    material: "Gümüş 925",
+    tags: ["üzük"]
+  },
+  {
+     image: "images/11.png",
+    price: "28 AZN",
+    material: "Gümüş 925",
+    tags: ["sep"]
+  },
+  {
+     image: "images/12.png",
+    price: "26 AZN",
+    material: "Gümüş 925",
+    tags: ["sep"]
+  },
+  {
+     image: "images/13.png",
+    price: "27 AZN",
+    material: "Gümüş 925",
+    tags: ["sep"]
+  },
+   {
+     image: "images/14.png",
+    price: "27 AZN",
+    material: "Gümüş 925",
+    tags: ["sep"]
+  },
+   {
+     image: "images/15.png",
+    price: "30 AZN",
+    material: "Gümüş 925",
+    tags: ["sep"]
+  },
+   {
+     image: "images/16.png",
+    price: "30 AZN",
+    material: "Gümüş 925",
+    tags: ["bilərzik"]
+  },
+  {
+     image: "images/17.png",
+    price: "24 AZN",
+    material: "Gümüş 925",
+    tags: ["üzük"]
+  },
+    {
+     image: "images/18.png",
+    price: "24 AZN",
+    material: "Gümüş 925",
+    tags: ["dəst","sırğa","sep","üzük"]
+  },
+    {
+     image: "images/19.png",
+    price: "24 AZN",
+    material: "Gümüş 925",
+    tags: ["sep"]
+  },
+    {
+     image: "images/20.png",
+    price: "26 AZN",
+    material: "Gümüş 925",
+    tags: ["sep"]
+  },
+  
+  // Öz məhsullarınızı bu formatda bura əlavə edin.
+  // Şəkillərinizi /images qovluğuna yerləşdirməyi unutmayın.
+];
+
+// İnstagram səhifənizin linkini bura daxil edin
+const instagramLink = "https://www.instagram.com/YOUR_SILVER_USERNAME_HERE/"; 
+
+// WhatsApp nömrənizin linkini bura daxil edin
+const whatsappLink = "https://wa.me/994XXYYYYYYY";
