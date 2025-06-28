4bb0d1bf0d9086bd53549ee07d0d4dcd9422caa7
Jamal Javadov <camalcavadovwork@gmail.com>
2025-06-28 13:44:43 +0400

    Update products.js

--- products.js
+++ products.js
@@ -5,121 +5,157 @@ const products = [
      image: "images/1.png",
     price: "45 AZN",
     material: "Gümüş 925",
-    tags: ["dəst", "bilərzik", "sep"]
+    tags: ["dəst", "bilərzik", "sep","qadın"]
   },
   {
      image: "images/2.png",
     price: "25 AZN",
     material: "Gümüş 925",
-    tags: ["üzük"]
+    tags: ["üzük","qadın"]
   },
   {
      image: "images/3.png",
     price: "26 AZN",
     material: "Gümüş 925",
-    tags: ["sep"]
+    tags: ["sep","qadın"]
   },
   {
      image: "images/4.png",
     price: "30 AZN",
     material: "Gümüş 925",
-    tags: ["bilərzik"]
+    tags: ["bilərzik","qadın"]
   },
   {
      image: "images/5.png",
     price: "75 AZN",
     material: "Gümüş 925",
-    tags: ["dəst","bilərzik","sep","üzük","sırğa"]
+    tags: ["dəst","bilərzik","sep","üzük","sırğa","qadın"]
   },
   {
      image: "images/6.png",
     price: "27 AZN",
     material: "Gümüş 925",
-    tags: ["üzük"]
+    tags: ["üzük","qadın"]
   },
   {
      image: "images/7.png",
     price: "25 AZN",
     material: "Gümüş 925",
-    tags: ["sırğa"]
+    tags: ["sırğa","qadın"]
   },
   {
      image: "images/8.png",
     price: "25 AZN",
     material: "Gümüş 925",
-    tags: ["üzük"]
+    tags: ["üzük","qadın"]
   },
   {
      image: "images/9.png",
     price: "25 AZN",
     material: "Gümüş 925",
-    tags: ["üzük"]
+    tags: ["üzük","qadın"]
   },
   {
      image: "images/10.png",
     price: "26 AZN",
     material: "Gümüş 925",
-    tags: ["üzük"]
+    tags: ["üzük","qadın"]
   },
   {
      image: "images/11.png",
     price: "28 AZN",
     material: "Gümüş 925",
-    tags: ["sep"]
+    tags: ["sep","qadın"]
   },
   {
      image: "images/12.png",
     price: "26 AZN",
     material: "Gümüş 925",
-    tags: ["sep"]
+    tags: ["sep","qadın"]
   },
   {
      image: "images/13.png",
     price: "27 AZN",
     material: "Gümüş 925",
-    tags: ["sep"]
+    tags: ["sep","qadın"]
   },
    {
      image: "images/14.png",
     price: "27 AZN",
     material: "Gümüş 925",
-    tags: ["sep"]
+    tags: ["sep","qadın"]
   },
    {
      image: "images/15.png",
     price: "30 AZN",
     material: "Gümüş 925",
-    tags: ["sep"]
+    tags: ["sep","qadın"]
   },
    {
      image: "images/16.png",
     price: "30 AZN",
     material: "Gümüş 925",
-    tags: ["bilərzik"]
+    tags: ["bilərzik","qadın"]
   },
   {
      image: "images/17.png",
     price: "24 AZN",
     material: "Gümüş 925",
-    tags: ["üzük"]
+    tags: ["üzük","qadın"]
   },
     {
      image: "images/18.png",
     price: "24 AZN",
     material: "Gümüş 925",
-    tags: ["dəst","sırğa","sep","üzük"]
+    tags: ["dəst","sırğa","sep","üzük","qadın"]
   },
     {
      image: "images/19.png",
     price: "24 AZN",
     material: "Gümüş 925",
-    tags: ["sep"]
+    tags: ["sep","qadın"]
   },
     {
      image: "images/20.png",
     price: "26 AZN",
     material: "Gümüş 925",
-    tags: ["sep"]
+    tags: ["sep","qadın"]
+  },
+      {
+     image: "images/21.png",
+    price: "26 AZN",
+    material: "Gümüş 925",
+    tags: ["üzük","qadın"]
+  },
+      {
+     image: "images/22.png",
+    price: "26 AZN",
+    material: "Gümüş 925",
+    tags: ["sep","qadın"]
+  },
+      {
+     image: "images/23.png",
+    price: "26 AZN",
+    material: "Gümüş 925",
+    tags: ["sep","qadın"]
+  },
+      {
+     image: "images/24.png",
+    price: "26 AZN",
+    material: "Gümüş 925",
+    tags: ["sep","qadın"]
+  },
+      {
+     image: "images/25.png",
+    price: "24 AZN",
+    material: "Gümüş 925",
+    tags: ["sep","qadın"]
+  },
+      {
+     image: "images/26.png",
+    price: "53 AZN",
+    material: "Gümüş 925",
+    tags: ["dəst","qadın","sırğa","üzük","sep"]
   },
   
   // Öz məhsullarınızı bu formatda bura əlavə edin.
